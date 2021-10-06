8ba970d643a601e7b6704cfe53d9d93273dbb5e3
Lennart Montañez <trannel04@gmail.com>
2021-10-05 22:01:07 -0500

    includig react-router

--- src/App.tsx
+++ src/App.tsx
@@ -4,21 +4,14 @@ import "./App.css";
 import Login from "./pages/login/login";
 
 import { SideBar } from "./pages/SideBar/SideBar";
-import { SalesMenu } from "./pages/SaleMenu/SalesMenu";
-
-import "./styles/commons/mixin.scss";
+//import { SalesMenu } from "./pages/SaleMenu/SalesMenu";
 
 function App() {
   return (
     <div className="App">
       {/* <Login /> */}
-      <div className="CanvasLayer">
-        <SideBar />
 
-        <main className="main">
-          <SalesMenu />
-        </main>
-      </div>
+      <SideBar />
     </div>
   );
 }
--- src/pages/SideBar/SideBar.tsx
+++ src/pages/SideBar/SideBar.tsx
@@ -2,13 +2,15 @@ import { TiTags } from "react-icons/ti";
 import { BiCube, BiUser } from "react-icons/bi";
 import { SiShopware } from "react-icons/si";
 
+import { Switch, Route, Link } from "react-router-dom";
+
+import { SalesMenu } from "../SaleMenu/SalesMenu";
+
 import "./SideBar.scss";
 
 export function SideBar() {
   return (
-    <div className="Canvas">
-      {/* <header className="Header"></header> */}
-
+    <div className="CanvasLayer">
       <aside className="Aside">
         <div className="AsideContent">
           <div className="LogoContainer">
@@ -17,27 +19,31 @@ export function SideBar() {
           <div className="ButtonRouters">
             <div className="Btn-Cont Cont_Sales">
               <TiTags className="IconRouter" size="3.5em" />
-              <button className="ButtonRouter">Ventas</button>
+              <Link className="ButtonRouter" to="Sales-Menu">
+                Ventas
+              </Link>
             </div>
             <div className="Btn-Cont Cont_Products">
               <BiCube className="IconRouter" size="3.5em" />
-              <button className="ButtonRouter">Productos</button>
+              <Link className="ButtonRouter" to="/">
+                Productos
+              </Link>
             </div>
             <div className="Btn-Cont Cont_Users">
               <BiUser className="IconRouter" size="3.5em" />
-              <button className="ButtonRouter">Usuarios</button>
+              <Link className="ButtonRouter" to="/">
+                Usuarios
+              </Link>
             </div>
           </div>
         </div>
       </aside>
 
-      {/* <main className="Main">
-        <Route path="/SalesMenu"></Route>
-        <Route path="/ProductsMenu"></Route>
-        <Route path="/UsersMenu"></Route>
-      </main> */}
-
-      {/* <footer className="Footer"></footer> */}
+      <main>
+        <Route path="/Sales-Menu">
+          <SalesMenu />
+        </Route>
+      </main>
     </div>
   );
 }
